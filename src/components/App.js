9807bdf74b1a4925352d66421be24586afbeedfc
lorenzoricci <lorenzo@amalgama.co>
2019-08-27 10:56:44 -0300

    api call refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,54 @@
-import React from 'react';
+import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
-    state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
-    onTermSubmit = async(term) => {
-       const response = await youtube.get('/search', {
-            params: {
-                q: term
-            }
+  componentDidMount() {
+    this.onSearchSubmit("cats");
+  }
+
+  onSearchSubmit = term => {
+    youtube
+      .get("/search", {
+        params: {
+          q: term
+        }
+      })
+      .then(response => {
+        this.setState({
+          videos: response.data.items,
+          selectedVideo: response.data.items[0]
         });
-       this.setState({ videos: response.data.items});
-    };
+      });
+  };
 
-    render() {
-        return (
-            <div className="ui container">
-                <SearchBar onFormSubmit={this.onTermSubmit} />
-                I have {this.state.videos.length} videos.
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video });
+  };
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onSearchSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
             </div>
-        );
-    }
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
